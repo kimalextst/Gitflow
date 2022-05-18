945ce8f8b215edd0bbf3b447312e101c883e080a
kimalextst <kimtorresst@gmail.com>
2022-05-18 15:59:21 -0300

    Corrigir bugs

--- src/listacompras/ListaCompras.kt
+++ src/listacompras/ListaCompras.kt
@@ -1,37 +1,47 @@
 package listacompras
 
+import menu.Menu.Companion.opcoes
+
 /*Aqui chamamos de lista de compras apenas para ter as funções específicas da lista de compras que o sistema exige,
 afinal é um sistema de lista de compras. Não confundir com o conceito de List do Kotlin*/
 class ListaCompras {
     companion object {
         fun informarQuantidadeVerduraGrao(alimento: String): Int {
-            println("Informe a quantidade de $alimento em gramas: ")
-            val input = readln()
-
-            if (input.isEmpty() || input.isBlank()) {
-                throw UnsupportedOperationException("Não é permitido inserir valor vazio")
-            }
-
             var gramas = 0
             try {
+                println("--------------------------------------------------------------------")
+                print("Informe a quantidade de $alimento em gramas: ")
+                val input = readln()
+                println("--------------------------------------------------------------------")
+
                 gramas = input.toInt()
                 if (gramas < 0) {
                     println("Não é permitido números negativos, tente novamente")
-                    informarQuantidadeVerduraGrao(alimento)
+                    opcoes(alimento)
                 }
 
-            } catch (exception: NumberFormatException) {
-                println("Não é permitido texto, somente número")
+            } catch (exception: Exception) {
+                println("É permitido somente números")
+                opcoes(alimento)
             }
             return gramas
         }
 
         fun informarNomeAlimento(alimento: String): String {
-            println("Informe o tipo $alimento a ser adicionado na lista: ")
-            val nome = readln()
+            var nome = ""
 
-            if (nome.isEmpty() || nome.isBlank()) {
-                throw UnsupportedOperationException("Não é permitido inserir valor vazio")
+            try {
+                println("--------------------------------------------------------------------")
+                print("Informe o tipo $alimento a ser adicionado na lista: ")
+                nome = readln()
+                println("--------------------------------------------------------------------")
+
+                if (nome.length < 2) {
+                    println("Formato inválido")
+                    opcoes(alimento)
+                }
+            } catch (ex : Exception){
+                opcoes(alimento)
             }
             return nome
         }
